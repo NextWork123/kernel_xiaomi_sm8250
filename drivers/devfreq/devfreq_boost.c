13699d99d778030e245264ade1540a09cd48c721
Emanuel <emanuelghub@gmail.com>
2021-08-14 12:01:00 +0400

    devfreq_boost: convert to state_notifier

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -6,7 +6,7 @@
 #define pr_fmt(fmt) "devfreq_boost: " fmt
 
 #include <linux/devfreq_boost.h>
-#include <linux/msm_drm_notify.h>
+#include <linux/state_notifier.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
@@ -31,7 +31,7 @@ struct boost_dev {
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
-	struct notifier_block msm_drm_notif;
+	struct notifier_block state_notif;
 };
 
 static void devfreq_input_unboost(struct work_struct *work);
@@ -180,27 +180,23 @@ static int devfreq_boost_thread(void *data)
 	return 0;
 }
 
-static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
-			  void *data)
+static int state_notifier_cb(struct notifier_block *nb,
+			       unsigned long action, void *data)
 {
-	struct df_boost_drv *d = container_of(nb, typeof(*d), msm_drm_notif);
+	struct df_boost_drv *d = container_of(nb, typeof(*d), state_notif);
 	int i;
-	struct msm_drm_notifier *evdata = data;
+	struct state_event *evdata = data;
 	int *blank = evdata->data;
-
-	/* Parse framebuffer blank events as soon as they occur */
-	if (action != MSM_DRM_EARLY_EVENT_BLANK)
-		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
 	for (i = 0; i < DEVFREQ_MAX; i++) {
 		struct boost_dev *b = d->devices + i;
 
-		if (*blank == MSM_DRM_BLANK_UNBLANK) {
+		if (*blank == STATE_NOTIFIER_BOOST) {
 			clear_bit(SCREEN_OFF, &b->state);
 			__devfreq_boost_kick_max(b,
 				CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
-		} else {
+		} else if (*blank == STATE_NOTIFIER_SUSPEND) {
 			set_bit(SCREEN_OFF, &b->state);
 			wake_up(&b->boost_waitq);
 		}
@@ -320,16 +316,12 @@ static int __init devfreq_boost_init(void)
 		goto stop_kthreads;
 	}
 
-	d->msm_drm_notif.notifier_call = msm_drm_notifier_cb;
-	d->msm_drm_notif.priority = INT_MAX;
-	if (lcd_active_panel) {
-		ret = drm_panel_notifier_register(lcd_active_panel, &d->msm_drm_notif);
-		if (ret) {
-			pr_err("Unable to register fb_notifier: %d\n", ret);
-			goto unregister_handler;
-		}
-	} else {
-		pr_err("lcd_active_panel is null\n");
+	d->state_notif.notifier_call = state_notifier_cb;
+	d->state_notif.priority = INT_MAX;
+	ret = state_register_client(&d->state_notif);
+	if (ret) {
+		pr_err("Failed to register state notifier, err: %d\n", ret);
+		goto unregister_handler;
 	}
 
 	return 0;
