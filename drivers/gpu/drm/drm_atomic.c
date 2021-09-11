6f956e4ce941f4a9bf1a8830b64e7012f496fcdd
Emanuel <emanuelghub@gmail.com>
2021-09-12 02:39:30 +0400

    Revert "devfreq_boost: Add support for boosting CPU_LLCC_BW device"
    This reverts commit 9cee852876d5b67c33ca73403dd059c454d1909b.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2604,9 +2604,7 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	/* Boost CPU and DDR when committing a new frame if sched_boost > 0 */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			sysctl_sched_boost) {
-		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW_DDR);
-		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
 	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
